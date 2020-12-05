2e02c0cbcb09b3a7e355840bf5c02f8b5f7bc650
99003154 <75408153+99003154@users.noreply.github.com>
2020-12-05 13:54:20 +0530

    Update calculator.h

--- inc/calculator.h
+++ inc/calculator.h
@@ -4,7 +4,7 @@
 *
 */
 
-#ifndef __CALCULATORS_H__
+#ifndef __CALCULATOR_H__
 #define __CALCULATOR_H__
 
 #include <stdio.h>
@@ -12,3 +12,18 @@
 #include <stdio_ext.h>
 
 int area_circle(float a);
+int area_square(int a);
+int area_triangle(int h, int b);
+int area_rectangle(int a, int b);
+int perimeter_square(int a);
+int perimeter_triangle(int a);
+int perimeter_rectangle(int l, int w);
+int perimeter_circle(float r);
+int celsius_fahrenheit(float c);
+int celsius_kelvin(float c);
+int fahrenheit_celsius(float f);
+int fahrenheit_kelvin(float f);
+int kelvin_celsius(float k);
+int kelvin_fahrenheit(float k);
+
+#endif  /* #define __CALCULATOR_H__ */
