7f69d06476f840036425603cda98e0dc48483faf
99003154 <75408153+99003154@users.noreply.github.com>
2020-12-05 13:11:17 +0530

    Create calculator.h

---
+++ inc/calculator.h
@@ -0,0 +1,14 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+
+#ifndef __CALCULATORS_H__
+#define __CALCULATOR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+int area_circle(float a)
