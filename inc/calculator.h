e416edcc49dabce7f729b7035c844dcea5a7cd18
99003155 <75408057+99003155@users.noreply.github.com>
2020-12-06 20:12:20 +0530

    Update calculator.h

--- inc/calculator.h
+++ inc/calculator.h
@@ -37,4 +37,24 @@ float do_miles_km(float input1);
 float do_miles_m(float input1);
 float do_km_miles(float input1);
 
+int add(int operand1, int operand2);
+int subtract(int operand1, int operand2);
+int multiply(int operand1, int operand2);
+int divide(int operand1, int operand2);
+int modulus(int operand1, int operand2);
+int square(int operand1);
+int cube(int operand1);
+//int squareroot(int operand1);
+int greater(int operand1, int operand2);
+int lesser(int operand1, int operand2);
+//int equal(int operand1, int operand2);
+int prime(int operand1);
+int odd(int operand1);
+int even(int operand1);
+int reverse(int operand1);
+//int si(int operand1, int operand2, int operand3);
+//int ci(int operand1,int operand2, int operand3);
+int profit(int operand1, int operand2);
+int loss(int operand1, int operand2);
+
 #endif  /* #define __CALCULATOR_H__ */
