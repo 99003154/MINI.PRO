078df6109ac83b9b605c3ac1c1837fb4d4154b0d
99003155 <75408057+99003155@users.noreply.github.com>
2020-12-06 20:31:37 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -20,8 +20,12 @@ float calculator_c1 = 0;
 int calculator_f = 0;
 int calculator_k = 0;
 
+int calculator_operand1 = 0;
+int calculator_operand2 = 0;
+int calculator_operand3 = 0;
+
 /* Valid operations */
-enum operations{ AREA_CIRCLE=1, AREA_SQUARE, AREA_TRIANGLE, AREA_RECTANGLE, PERIMETER_SQUARE,  PERIMETER_TRIANGLE,  PERIMETER_RECTANGLE,  PERIMETER_CIRCLE, CELSIUS_FAHRENHEIT, CELSIUS_KELVIN, FAHRENHEIT_CELSIUS,FAHRENHEIT_KELVIN, KELVIN_CELSIUS, KELVIN_FAHRENHEIT, EXIT };
+enum operations{ AREA_CIRCLE=1, AREA_SQUARE, AREA_TRIANGLE, AREA_RECTANGLE, PERIMETER_SQUARE,  PERIMETER_TRIANGLE,  PERIMETER_RECTANGLE,  PERIMETER_CIRCLE, CELSIUS_FAHRENHEIT, CELSIUS_KELVIN, FAHRENHEIT_CELSIUS,FAHRENHEIT_KELVIN, KELVIN_CELSIUS, KELVIN_FAHRENHEIT, ADD, SUBTRACT, MULTIPLY, DIVIDE, MODULUS, SQUARE, CUBE, SQUAREROOT, GREATER, LESSER, EQUAL, PRIME, ODD, EVEN, REVERSE, SI, CI, PROFIT, LOSS, EXIT };
 
 /* Display the menu of operations supported */
 void calculator_menu(void);
@@ -42,7 +46,7 @@ int main(int argc, char *argv[])
 void calculator_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Area_cirlce\n2. Area_square\n3. Area_triangle\n4. Area_rectangle\n5. Perimeter_square\n6. Perimeter_triangle\n7. Perimeter_rectangle\n8. Perimeter_circle\n9. Celsius_fahrenheit\n10. Celsius_kelvin\n11. Fahrenheit_celsius\n12. Fahrenheit_kelvin\n13. Kelvin_celsius\n14. Kelvin_fahrenheit\n15.  Exit");
+    printf("\n1. Area_cirlce\n2. Area_square\n3. Area_triangle\n4. Area_rectangle\n5. Perimeter_square\n6. Perimeter_triangle\n7. Perimeter_rectangle\n8. Perimeter_circle\n9. Celsius_fahrenheit\n10. Celsius_kelvin\n11. Fahrenheit_celsius\n12. Fahrenheit_kelvin\n13. Kelvin_celsius\n14. Kelvin_fahrenheit\n15. Add\n16. Subtract\n17. Multiply\n18. Divide\n19. Modulus\n20. square\n21. cube\n22. squareroot\n23. greater\n24. lesser\n25. equal\n26. prime\n27. odd\n28. even\n29. reverse\n30. si\n31. ci\n32. profit\n33. loss\n34 Exit");
     printf("\n\tEnter your choice\n");
    
      __fpurge(stdin);
@@ -58,7 +62,7 @@ void calculator_menu(void)
     {
         printf("\n\tEnter your Numbers with space between them\n");
         __fpurge(stdin);
-        scanf("%f %f %f %d %d %d %d %d %d %d %d", &calculator_a, &calculator_r, &calculator_c1, &calculator_b, &calculator_h, &calculator_c, &calculator_l, &calculator_w, &calculator_a1, &calculator_f, &calculator_k);
+        scanf("%f %f %f %d %d %d %d %d %d %d %d %d %d %d", &calculator_a, &calculator_r, &calculator_c1, &calculator_b, &calculator_h, &calculator_c, &calculator_l, &calculator_w, &calculator_a1, &calculator_f, &calculator_k, &calculator_operand1, &calculator_operand2, &calculator_operand3);
     }
     else
     {
@@ -191,7 +195,161 @@ void calculator_menu(void)
             getchar();
             break;
             
-        case 31:
+            case ADD:
+            printf("\n\t%d + %d= %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            add(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case SUBTRACT:
+            printf("\n\t%d- %d= %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            subtract(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case MULTIPLY:
+            printf("\n\t%d* %d = %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            multiply(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case DIVIDE:
+            printf("\n\t%d / %d = %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            divide(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case MODULUS:
+            printf("\n\t modulus  =%d\nEnter to continue",
+
+            modulus(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case SQUARE:
+            printf("\n\t square %d =%d\nEnter to continue",
+            calculator_operand1,
+            square(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+            case CUBE:
+            printf("\n\t cube %d =%d\nEnter to continue",
+            calculator_operand1,
+            cube(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        /*case SQUAREROOT:
+            printf("\n\t squareroot %d =%d\nEnter to continue",
+            calculator_operand1,
+            squareroot(calculator_operand1));
+            __fpurge(stdin);
+            getchar();
+            break;*/
+        case GREATER:
+            printf("\n\t Greater %d and %d = %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            greater(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case LESSER:
+            printf("\n\t Lesser %d and %d = %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            lesser(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        /*case EQUAL:
+            printf("\n\t Equal %d and %d= %d\nEnter to continue",
+            calculator_operand1,
+            calculator_operand2,
+            equal(calculator_operand1, calculator_operand2));
+            __fpurge(stdin);
+            getchar();
+            break;*/
+         case PRIME:
+            printf("\n\t Prime %d=%d\nEnter to continue",
+            calculator_operand1,
+            prime(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case ODD:
+            printf("\n\t Odd %d =%d\nEnter to continue",
+            calculator_operand1,
+            odd(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case EVEN:
+            printf("\n\t Even %d =%d\nEnter to continue",
+            calculator_operand1,
+            even(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case REVERSE:
+            printf("\n\t Reverse %d =%d\nEnter to continue",
+            calculator_operand1,
+            reverse(calculator_operand1));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        /*case SI:
+            printf("\n\t simple interest = %d\nEnter to continue",
+            si(calculator_operand1, calculator_operand2, calculator_operand3));
+            __fpurge(stdin);
+            getchar();
+            break;
+        case CI:
+            printf("\n\t compound interest = %d\nEnter to continue",
+            ci(calculator_operand1, calculator_operand2, calculator_operand3));
+            __fpurge(stdin);
+            getchar();
+            break;*/
+        case PROFIT:
+            printf("\n\t profit = %d\nEnter to continue",
+
+            profit(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+        case LOSS:
+            printf("\n\t loss = %d\nEnter to continue",
+
+            loss(calculator_operand1, calculator_operand2));
+
+            __fpurge(stdin);
+            getchar();
+            break;
+            
+        case 34:
             exit(0);
             break;
         default:
